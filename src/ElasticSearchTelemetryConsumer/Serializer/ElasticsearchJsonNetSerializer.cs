83b3967faf70fb3c455edc0249b91bdefef1baac
Alex McCool <alex.mccool@yahoo.com>
2020-08-31 17:05:38 -0700

    Upgrade ES client to 6.8 (#17)
    * updating Elasticsearch.Net to 6.8.2 (#9)
    
    * Create dotnet-core.yml (#10)
    
    * Create dotnet-core.yml
    
    * Update ESTester.csproj
    
    * needs windows for es extract
    
    * pack a nuget
    
    * upload nugets (#11)
    
    * Create dotnet-core.yml
    
    * Update ESTester.csproj
    
    * needs windows for es extract
    
    * pack a nuget
    
    * upload nugets
    
    * pathing
    
    * we already have the nupkg
    
    * only release
    
    * do the version number directly in the build
    
    * pack all
    
    Co-authored-by: Brian Stauff <brian.stauff.github@gmail.com>

--- src/ElasticSearchTelemetryConsumer/Serializer/ElasticsearchJsonNetSerializer.cs
+++ src/ElasticSearchTelemetryConsumer/Serializer/ElasticsearchJsonNetSerializer.cs
@@ -14,84 +14,83 @@ namespace Orleans.TelemetryConsumers.ElasticSearch.Serializer
 {
 	public class ElasticsearchJsonNetSerializer : IElasticsearchSerializer
 	{
-		private static readonly Encoding ExpectedEncoding = new UTF8Encoding(false);
-		/// <summary>
-		/// The size of the buffer to use when writing the serialized request
-		/// to the request stream
-		/// Performance tests as part of https://github.com/elastic/elasticsearch-net/issues/1899 indicate this 
-		/// to be a good compromise buffer size for performance throughput and bytes allocated.
-		/// </summary>
-		protected virtual int BufferSize => 1024;
+        private static readonly Encoding ExpectedEncoding = new UTF8Encoding(false);
 
-		private readonly JsonSerializerSettings _settings;
-		private JsonSerializer _defaultSerializer;
+        /// <summary>
+        /// The size of the buffer to use when writing the serialized request
+        /// to the request stream
+        /// Performance tests as part of https://github.com/elastic/elasticsearch-net/issues/1899 indicate this
+        /// to be a good compromise buffer size for performance throughput and bytes allocated.
+        /// </summary>
+        protected virtual int BufferSize => 1024;
 
-		public ElasticsearchJsonNetSerializer(IList<JsonConverter> converters = null)
-		{
-			//_settings = settings ?? CreateSettings();
-			_settings = CreateSettings();
+        private readonly JsonSerializerSettings _settings;
+        private JsonSerializer _defaultSerializer;
 
-			if (converters != null)
-			{
-				if (_settings.Converters != null)
-				{
-					foreach (var converter in converters)
-					{
-						_settings.Converters.Add(converter);
-					}
-				}
-				else
-				{
-					_settings.Converters = converters;
-				}
-			}
+        public ElasticsearchJsonNetSerializer(JsonSerializerSettings settings = null)
+        {
+            _settings = settings ?? CreateSettings();
+            this._defaultSerializer = JsonSerializer.Create(_settings);
+        }
 
-			this._defaultSerializer = JsonSerializer.Create(_settings);
-		}
+        private JsonSerializerSettings CreateSettings()
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                DefaultValueHandling = DefaultValueHandling.Include,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+            return settings;
+        }
 
-		private JsonSerializerSettings CreateSettings()
-		{
-			var settings = new JsonSerializerSettings()
-			{
-				DefaultValueHandling = DefaultValueHandling.Include,
-				NullValueHandling = NullValueHandling.Ignore,
-			};
-			return settings;
-		}
+        public object Deserialize(Type type, Stream stream)
+        {
+            var settings = this._settings;
+            return _Deserialize(type, stream, settings);
+        }
 
+        public T Deserialize<T>(Stream stream)
+        {
+            var settings = this._settings;
+            return (T)_Deserialize(typeof(T), stream, settings);
+        }
 
-		public T Deserialize<T>(Stream stream)
-		{
-			var settings = this._settings;
-			return _Deserialize<T>(stream, settings);
-		}
+        public Task<object> DeserializeAsync(Type type, Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(_Deserialize(type, stream));
+        }
 
-		public async Task<T> DeserializeAsync<T>(Stream responseStream, CancellationToken cancellationToken = new CancellationToken())
-		{
-			return this.Deserialize<T>(responseStream);
-		}
+        protected internal object _Deserialize(Type type, Stream stream, JsonSerializerSettings settings = null)
+        {
+            settings = settings ?? this._settings;
+            var serializer = JsonSerializer.Create(settings);
+            var jsonTextReader = new JsonTextReader(new StreamReader(stream));
+            var t = serializer.Deserialize(jsonTextReader, type);
+            return t;
+        }
 
-		protected internal T _Deserialize<T>(Stream stream, JsonSerializerSettings settings = null)
-		{
-			settings = settings ?? this._settings;
-			var serializer = JsonSerializer.Create(settings);
-			var jsonTextReader = new JsonTextReader(new StreamReader(stream));
-			var t = (T)serializer.Deserialize(jsonTextReader, typeof(T));
-			return t;
-		}
+        public Task<T> DeserializeAsync<T>(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var result = this.Deserialize<T>(stream);
+            return Task.FromResult(result);
+        }
 
+        public void Serialize<T>(T data, Stream stream, SerializationFormatting formatting = SerializationFormatting.Indented)
+        {
+            using (var writer = new StreamWriter(stream, ExpectedEncoding, BufferSize, leaveOpen: true))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                _defaultSerializer.Serialize(jsonWriter, data);
+                writer.Flush();
+                jsonWriter.Flush();
+            }
+        }
 
-		public void Serialize(object data, Stream writableStream, SerializationFormatting formatting = SerializationFormatting.Indented)
-		{
-			using (var writer = new StreamWriter(writableStream, ExpectedEncoding, BufferSize, leaveOpen: true))
-			using (var jsonWriter = new JsonTextWriter(writer))
-			{
-				_defaultSerializer.Serialize(jsonWriter, data);
-				writer.Flush();
-				jsonWriter.Flush();
-			}
-		}
-
-		public IPropertyMapping CreatePropertyMapping(MemberInfo memberInfo) => null;
-	}
+        public Task SerializeAsync<T>(T data, Stream stream, SerializationFormatting formatting = SerializationFormatting.Indented, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Serialize(data, stream, formatting);
+            return Task.CompletedTask;
+        }
+    }
 }
